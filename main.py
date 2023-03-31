9adf229c4e30a1be6f200f64ee448f4e5a0f8bf2
Przemek Dudek <przemod77@gmail.com>
2023-04-01 00:18:49 +0200

    QLearn: Uzupełniłem GUI o wartości tablicy wag

--- main.py
+++ main.py
@@ -14,23 +14,39 @@ root.title("DeepQ")
 frm = tkinter.Frame(root)
 
 
-def draw(status):
+def get_description(number, final_pass):
+    text = "Pass no."
+    if final_pass:
+        return " FINAL "
+
+    return text + str(number)
+
+
+def draw(status, description):
     frm.grid()
+
+    tkinter.Label(frm, text=description, font=("Helvetica", 20)).grid(column=0, row=0)
+    tkinter.Label(frm, text="L-weight", font=("Helvetica", 20)).grid(column=0, row=1)
+    tkinter.Label(frm, text="R-weight", font=("Helvetica", 20)).grid(column=0, row=2)
+    tkinter.Label(frm, text="E="+str("{:.2f}".format(EPSILON)), font=("Helvetica", 20)).grid(column=8, row=1, rowspan=2)
 
     for i in range(6):
         if i == status:
-            tkinter.Label(frm, text="☒", font=("Helvetica", 100)).grid(column=i, row=0)
+            tkinter.Label(frm, text="☒", font=("Helvetica", 100)).grid(column=i+1, row=0)
             continue
 
-        tkinter.Label(frm, text="☐", font=("Helvetica", 100)).grid(column=i, row=0)
+        tkinter.Label(frm, text="☐", font=("Helvetica", 100)).grid(column=i+1, row=0)
+
+        tkinter.Label(frm, text=str("{:.1e}".format(array[0][i])), font=("Helvetica", 20)).grid(column=i + 1, row=1)
+        tkinter.Label(frm, text=str("{:.1e}".format(array[1][i])), font=("Helvetica", 20)).grid(column=i + 1, row=2)
 
     if status != 7:
-        tkinter.Label(frm, text="☑", font=("Helvetica", 100)).grid(column=7, row=0)
+        tkinter.Label(frm, text="☑", font=("Helvetica", 100)).grid(column=8, row=0)
     else:
-        tkinter.Label(frm, text="☒", font=("Helvetica", 100)).grid(column=7, row=0)
+        tkinter.Label(frm, text="☒", font=("Helvetica", 100)).grid(column=8, row=0)
 
     frm.update()
-    time.sleep(0.25)
+    time.sleep(0.10)
 
 
 def get_reward(state):
@@ -48,7 +64,7 @@ def next_move(state):
         return -1
 
     if array[0][state] == 0 and array[1][state] == 0:
-        if randint(0, 1) == 1:
+        if random.randint(0, 1) == 1:
             return 1
 
         return -1
@@ -67,11 +83,13 @@ def calculate_reward(move, state, next_state):
     array[move][state] = array[move][state] + ALPHA*(get_reward(next_state) + (GAMMA*max(array[0][next_state], array[1][next_state])) - array[move][state])
 
 
-def one_pass():
+def one_pass(final_pass):
     state = 0
+    global number_of_steps
 
+    number_of_steps += 1
     while state != 6:
-        draw(state)
+        draw(state, get_description(number_of_steps, final_pass))
 
         move = next_move(state)
         next_state = max(state+move, 0)
@@ -80,18 +98,21 @@ def one_pass():
 
         state = next_state
 
-    draw(7)
+        if final_pass:
+            time.sleep(0.4)
+
+    draw(7, get_description(number_of_steps, final_pass))
+
 
 number_of_steps = 0
 
-while max(array[0][0], array[1][0]) == 0:
-    one_pass()
-    EPSILON = EPSILON - 0.1
-    number_of_steps = number_of_steps + 1
+while EPSILON > 0:
+    one_pass(False)
+    EPSILON = EPSILON - 0.15
 
-print("number of passes = ", number_of_steps)
-print("Weights L = ", array[0])
-print("Weights P = ", array[1])
+EPSILON = 0
+
+one_pass(True)
 
 root.mainloop()
 
